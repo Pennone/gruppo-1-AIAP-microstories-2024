5b0d82339a8ef413fb63f1b951e8a859c5d0bffc
matte98600 <matteo98600@gmail.com>
2024-02-09 11:08:20 +0100

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,11 +47,13 @@ $(document).ready(function () {
 
             var elementoTrovato = trovaElementoPerId(database, soloNumero);
 
-            var popoverHtml = '<div class="popover-content padding-10">';
-            popoverHtml += '<span class="pill flex maiuscola">' + elementoTrovato.animal + '</span>';
-            popoverHtml += '<div class="flex padding-10"><img src="' + elementoTrovato.img + '" alt="' + elementoTrovato.title + '" class="centra"></div>';
-            popoverHtml += '<h4>' + elementoTrovato.title + '</h4>';
-            popoverHtml += '</div>';
+            try {
+                var popoverHtml = '<div class="popover-content padding-10">';
+                popoverHtml += '<span class="pill flex maiuscola">' + elementoTrovato.animal + '</span>';
+                popoverHtml += '<div class="flex padding-10"><img src="' + elementoTrovato.img + '" alt="' + elementoTrovato.title + '" class="centra"></div>';
+                popoverHtml += '<h4>' + elementoTrovato.title + '</h4>';
+                popoverHtml += '</div>';
+            } catch { };
 
             $('body').append(popoverHtml);
 
@@ -103,9 +105,19 @@ $(document).ready(function () {
         var elementoTrovato = trovaElementoPerId(database, soloNumero);
         saveToSessionStorage("artefatto_temp", elementoTrovato);
 
-        if (navigazione == "home" || navigazione == "info-home2") {
-            navigazione = "info-home";
-            naviga(home_id);
+        var navigazione2 = getFromSessionStorage("navigazione_temp");
+
+        if (navigazione2 != "surreal" && navigazione2 != "pictographic" && navigazione2 != "realistic") {
+            if (navigazione == "home" || navigazione == "info-home2") {
+                navigazione = "info-home";
+                naviga(home_id);
+            }
+        } else if (navigazione2 == "surreal" || navigazione2 == "pictographic" || navigazione2 == "realistic") {
+            $("#3tab_home").removeClass("active");
+            $("#3tab_surreal").removeClass("active");
+            $("#3tab_pictographic").removeClass("active");
+            $("#3tab_realistic").removeClass("active");
+            $("#contenuto-tab").load("assets/data/route-info.html");
         }
     });
 
@@ -126,6 +138,8 @@ function trovaElementoPerId(array, idDaCercare) {
 
 function naviga(id) {
 
+    var navigazione2 = getFromSessionStorage("navigazione_temp");
+
     if (navigazione == "home") {
         $("#sceltapercorso > div").html(" "); //Svuota div
         $("#sceltapercorso").css("flex-basis", "40%");
@@ -136,7 +150,6 @@ function naviga(id) {
         $("#menudestro > h3").text("Explore freely");
         $("#menudestro > img").addClass("ruota180");
 
-
         setTimeout(function () {
             $("#sceltapercorso").addClass("bordo-lf");
         }, 50);
@@ -146,6 +159,11 @@ function naviga(id) {
             $("#sceltapercorso > div").show();
         }, 500);
     } else if (navigazione == "menù") {
+        if (navigazione2 !== null) {
+            removeFromSessionStorage("navigazione_temp");
+        }
+
+
         $("#sceltapercorso > div").html(" "); //Svuota div
         $("#sceltapercorso").css("flex-basis", "0%");
         $("#sceltapercorso2").css("flex-basis", "100%");
@@ -160,6 +178,11 @@ function naviga(id) {
         }, 500);
         navigazione = "home";
     } else if (navigazione == "route") {
+
+        if (navigazione2 !== null) {
+            removeFromSessionStorage("navigazione_temp");
+        }
+        
         $("#sceltapercorso > div").load("assets/data/chose-route.html");
 
         $("#XXXinterpretation").addClass("nascosto");
@@ -197,6 +220,11 @@ function naviga(id) {
         }, 500);
         navigazione = "info-home2";
     } else if (navigazione == "info-home2") {
+
+        if (navigazione2 !== null) {
+            removeFromSessionStorage("navigazione_temp");
+        }
+
         $("#sceltapercorso > div").load("assets/data/chose-route.html");
 
         $("#menudestro > h3").text("Explore freely");
@@ -279,6 +307,7 @@ function caricamento_pag(elemento) {
 function caricamento_tab(tab) {
     switch (tab) {
         case "3tab_home":
+            saveToSessionStorage("tab_temp", "3tab_home");
             $("#contenuto-tab").load("assets/data/3home.html");
 
             $("#3tab_home").addClass("active");
@@ -292,6 +321,7 @@ function caricamento_tab(tab) {
 
             break;
         case "3tab_surreal":
+            saveToSessionStorage("tab_temp", "3tab_surreal");
             $("#contenuto-tab").load("assets/data/surreal.html");
 
             $("#3tab_home").removeClass("active");
@@ -305,6 +335,7 @@ function caricamento_tab(tab) {
 
             break;
         case "3tab_pictographic":
+            saveToSessionStorage("tab_temp", "3tab_pictographic");
             $("#contenuto-tab").load("assets/data/pictographic.html");
 
             $("#3tab_home").removeClass("active");
@@ -318,6 +349,7 @@ function caricamento_tab(tab) {
 
             break;
         case "3tab_realistic":
+            saveToSessionStorage("tab_temp", "3tab_realistic");
             $("#contenuto-tab").load("assets/data/realistic.html");
 
             $("#3tab_home").removeClass("active");
@@ -341,51 +373,48 @@ var database = getFromSessionStorage("database");
 function getUniqueAndSortedAuthors(database) {
     var authors = [];
     $.each(database, function (index, item) {
-      if ($.inArray(item.author, authors) === -1) {
-        authors.push(item.author);
-      }
+        if ($.inArray(item.author, authors) === -1) {
+            authors.push(item.author);
+        }
     });
     // Ordina gli autori in ordine alfabetico
     authors.sort();
     return authors;
-  }
+}
 
 // Funzione per popolare il select con i valori degli autori
 function populateAuthorSelect(authors) {
     var select = $("#autore");
     $.each(authors, function (index, author) {
-      // Se l'autore è "sconosciuto", visualizza la stringa appropriata
-      var displayText = (author === "unknown") ? "Sconosciuto" : author;
-      select.append("<option value='" + author + "'>" + displayText + "</option>");
+        // Se l'autore è "sconosciuto", visualizza la stringa appropriata
+        var displayText = (author === "unknown") ? "Sconosciuto" : author;
+        select.append("<option value='" + author + "'>" + displayText + "</option>");
     });
-  }
+}
 
 function getUniqueAndSortedYears(database) {
     var years = [];
     $.each(database, function (index, item) {
-      if ($.inArray(item.year, years) === -1) {
-        years.push(item.year);
-      }
+        if ($.inArray(item.year, years) === -1) {
+            years.push(item.year);
+        }
     });
     // Ordina gli anni in modo cronologico
     years.sort(function (a, b) {
-      return a - b;
+        return a - b;
     });
     return years;
-  }
+}
 
 // Funzione per popolare il select con i valori degli anni
 function populateYearSelect(years) {
     var select = $("#data");
     $.each(years, function (index, year) {
-      // Se l'anno è 0, visualizza "sconosciuto", altrimenti visualizza l'anno
-      var displayText = (year === 0) ? "Sconosciuto" : year;
-      select.append("<option value='" + year + "'>" + displayText + "</option>");
+        // Se l'anno è 0, visualizza "sconosciuto", altrimenti visualizza l'anno
+        var displayText = (year === 0) ? "Sconosciuto" : year;
+        select.append("<option value='" + year + "'>" + displayText + "</option>");
     });
-  }
-
-
-
+}
 
 // Salvare dati in sessionStorage
 
